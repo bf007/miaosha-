41433c934b4d7e313afb726583f2b7c63310cfaa
bf007 <18614040398@163.com>
2019-01-17 23:38:01 +0800

    配置文件生成

--- src/main/java/com/miaoshaproject/App.java
+++ src/main/java/com/miaoshaproject/App.java
@@ -1,7 +1,12 @@
 package com.miaoshaproject;
 
+import com.miaoshaproject.dao.UserDOMapper;
+import com.miaoshaproject.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 
-@EnableAutoConfiguration
+
+//SpringBootApplication指定为主启动类，scanBasePackages将秒杀根目录下的包依次扫描，通过注解的方式自动发现spring特定的注解
+@SpringBootApplication(scanBasePackages = {"com.miaoshaproject"})
 @RestController
+@MapperScan("com.miaoshaproject.dao")
 public class App {
+
+    @Autowired
+    private UserDOMapper userDOMapper;
 
     @RequestMapping("/")
     public String home(){
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO == null){
+            return "用户对象不存在！";
 
-        return "Hello World";
+        }else{
+            return userDO.getName();
+        }
+
+
     }
 
 
